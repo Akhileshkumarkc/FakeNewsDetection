3a8694a8bbccd94c2994c9edd358a0ff0b52d41e
Vinaya Ganiga <ganigavinaya@gmail.com>
2017-12-14 18:22:30 -0600

    Update streaming.py

--- streaming.py
+++ streaming.py
@@ -4,45 +4,61 @@ from pyspark.streaming.kafka import KafkaUtils
 import json
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 
 def filterAndGetUrl(tweet):
-    if(tweet['user']["url"]==None or tweet['user']["lang"]!='en'):
+    if(tweet['entities']["urls"]==None or len(tweet['entities']["urls"])==0 or tweet['user']["lang"]!='en'):
         return False
     
-    if("youtube" in tweet['user']["url"] or "youtu.be" in tweet['user']["url"] or "twitter" in tweet['user']["url"]):
-        return False
-    else:
-        return True
+    return True
 
 
-def getUrlText(url):  
-    try:
-        with urlopen(url) as response:
-            html = response.read()
-            
-#            soupHeader = BeautifulSoup(html).find(['h1','h2','h3','h4'])
-#            header = ""
-#            for h in soupHeader:
-#                data = h.get_text()
-#                if(data!=""):
-#                    header += ' '+data
-                    
-            soup = BeautifulSoup(html,'html.parser').body
-            
-            divs = soup.find_all('div')
+def getUrlText(urls):  
+    finaltext = ""
+    finalHeader = ""
+    
+    for url in urls:
+        link = url["expanded_url"]
+        
+        if(link==None or "youtube" in link or "youtu.be" in link or "twitter" in link):
+            continue
+        try:
             text = ""
-            for div in divs:
-                data = div.stripped_strings
-                for string in data:
-                    if(string!=""):
-                        text += ' '+string
-#            return [header,text]
-            return text
+            title = ""
+            with urlopen(link) as response:
+                html = response.read()
+                       
+                soup = BeautifulSoup(html,'html.parser').body
+                
+                header = soup.find_all('h1')
+                
+                for h1 in header:
+                    title+=h1.text+" "
+                
+                if(h1==""):
+                    header = soup.find_all('h2')
+                
+                    for h2 in header:
+                        title+=h2.text     
+                        
+                paragraphs = soup.find_all(['p','div'])
+                
+                for p in paragraphs:                
+                    text+=p.text+" "                   
+                        
+                finaltext += re.sub('[^0-9a-zA-Z]+', ' ', text) + "\n" 
+                finalHeader += re.sub('[^0-9a-zA-Z]+', ' ', title) + "\n" 
+        except:
+            pass
+    
+    return [finalHeader,finaltext]
 
-    except:
-        return ""
-    return ''
+def formText(text):    
+    retText = re.sub(r'RT.+:\s+', '', text)  
+    retText = re.sub('[^0-9a-zA-Z]+', ' ', retText)
+    retText = re.sub(r'https?:\/\/.*[\r\n]*', '', retText)
+    return retText
 
 if __name__ == "__main__":
 
@@ -60,7 +76,7 @@ if __name__ == "__main__":
     parsed = kafkaStream.map(lambda v: json.loads(v[1]))
 
     
-    urls = parsed.filter(filterAndGetUrl).map(lambda tweet : getUrlText(tweet['user']["url"]))
+    urls = parsed.filter(filterAndGetUrl).map(lambda tweet : [formText(tweet['text']),getUrlText(tweet['entities']["urls"])])
 
 	#Print the User tweet counts
     urls.pprint()
